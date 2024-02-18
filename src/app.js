f3b91679cc83109dec25b5e8f2bd9b6373f9d85e
Manuel García <153007701+delafter@users.noreply.github.com>
2024-02-18 10:42:30 +0000

    end

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//write your code here
+let pronoun = ["La", "Nuestra"];
+let adj = ["super", "gran"];
+let noun = ["pagina", "bestia"];
+//};
+
+function dominios(datos) {
+  return datos[Math.floor(Math.random() * datos.length)];
+}
+for (let i = 0; i < 10; i++) {
+  document.write(
+    `${dominios(pronoun)}${dominios(adj)}${dominios(noun)}.com<br>`
+  );
+  console.log(`${dominios(pronoun)}${dominios(adj)}${dominios(noun)}.com`);
+}
